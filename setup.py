df5607068ca9e7cdc03e2d15c498f41fb0d76b02
Valerio Zhang <valerio.zhang@mdland.com>
2019-10-25 16:05:39 -0400

    bulk insert fixes

--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ extras = {
 }
 
 setup(
-    name='d6tstack',
-    version='0.2.0',
+    name='d6tstackmod',
+    version='1',
     packages=['d6tstack'],
-    url='https://github.com/d6t/d6tstack',
+    url='https://github.com/valeriozhang/d6tstack/archive/1.tar.gz',
     license='MIT',
     author='DataBolt Team',
-    author_email='support@databolt.tech',
-    description='d6tstack: Quickly ingest CSV and XLS files. Export to pandas, SQL, parquet',
+    author_email='valeriozhang14@gmail.com',
+    description='d6tstackmod: Quickly ingest CSV and XLS files. Export to pandas, SQL, parquet',
     long_description='Quickly ingest raw files. Works for XLS, CSV, TXT which can be exported to CSV, Parquet, SQL and Pandas. d6tstack solves many performance and schema problems typically encountered when ingesting raw files.',
     install_requires=[
         'numpy','pandas>=0.22.0','sqlalchemy','scipy','d6tcollect'
@@ -23,6 +23,6 @@ setup(
     extras_require=extras,
     include_package_data=True,
     python_requires='>=3.5',
-    keywords=['d6tstack', 'ingest csv'],
+    keywords=['d6tstackmod', 'ingest csv'],
     classifiers=[]
 )
